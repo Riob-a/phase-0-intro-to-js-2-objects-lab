75f4b1057039636b8bfd21b979d524f9c19f3a5a
Derrick <riobad65@gmail.com>
2024-03-22 19:09:22 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee ={
+    name:`Derrick`,
+    streetAddress: `11 Broadway`
+};
+//
+function updateEmployeeWithKeyAndValue(employee,name,Derrick){
+    return {
+        ...employee,
+        [name]: Derrick,
+    }; 
+}
+
+//
+function destructivelyUpdateEmployeeWithKeyAndValue(employee){
+employee.streetAddress ="12 Broadway";
+return employee;
+}
+
+//
+function deleteFromEmployeeByKey(employee){
+const {name, ...newemployee} = employee;
+return newemployee;
+}
+
+//
+function destructivelyDeleteFromEmployeeByKey(employee){
+ employee.name = undefined;
+ return employee;
+}
